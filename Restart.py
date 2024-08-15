9c456e618a0c15a06b03c2f4763f135848cb761b
anal <analshah1705@gmail.com>
2024-08-14 18:07:25 -0700

    windows resstart
    add windows function

--- Restart.py
+++ Restart.py
@@ -1,4 +1,6 @@
 import SystemRecognition
 import os
+if SystemRecognition.SystemRecognition() == 'Windows':
+    print("shutdown")
 if SystemRecognition.SystemRecognition() == 'Darwin':
     os.system("sudo shutdown -h 23:30")
