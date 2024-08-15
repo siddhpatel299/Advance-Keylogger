8df2692b30e45901417d16a9ae0629ff2907dbc2
siddhpatel299 <siddhpatel2992001@gmail.com>
2024-08-14 18:12:06 -0700

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -2,8 +2,5 @@ import SystemRecognition
 import os
 if SystemRecognition.SystemRecognition() == 'Windows':
     print("shutdown")
-if SystemRecognition.SystemRecognition() == 'Linux':
-    print("shutdown")
 if SystemRecognition.SystemRecognition() == 'Darwin':
-    os.system("sudo shutdown -h 23:30")
-print("Hello this is from Siddh")
+    print("shutdown")
