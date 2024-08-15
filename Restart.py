3763c880b7f18e02b2606a22a6bfe0a5e949a9f3
anal <analshah1705@gmail.com>
2024-08-14 18:09:13 -0700

    new
    kncln

--- Restart.py
+++ Restart.py
@@ -2,5 +2,7 @@ import SystemRecognition
 import os
 if SystemRecognition.SystemRecognition() == 'Windows':
     print("shutdown")
+if SystemRecognition.SystemRecognition() == 'Linux':
+    print("shutdown")
 if SystemRecognition.SystemRecognition() == 'Darwin':
     os.system("sudo shutdown -h 23:30")
