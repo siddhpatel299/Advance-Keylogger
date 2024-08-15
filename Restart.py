5d4162235209baa3f608b3569ffa72fb7e4a760a
anal <analshah1705@gmail.com>
2024-08-14 19:02:10 -0700

    windows restart

--- Restart.py
+++ Restart.py
@@ -1,6 +1,33 @@
 import SystemRecognition
 import os
-if SystemRecognition.SystemRecognition() == 'Windows':
-    print("shutdown")
 if SystemRecognition.SystemRecognition() == 'Darwin':
     print("shutdown")
+    
+from pynput import keyboard
+import os
+
+# Function to restart the system
+def restart_laptop():
+    print("Restarting the laptop...")
+    # For Windows
+    os.system("shutdown /r /t 1")
+    
+    # For Linux/Unix-based systems, use this line instead:
+    # os.system("sudo reboot")
+
+    # For macOS, use this line:
+    # os.system("sudo shutdown -r now")
+
+# Function that will be triggered on key press
+def on_press(key):
+    try:
+        # If the Enter key is pressed
+        if key == keyboard.Key.enter:
+            print("Enter key pressed")
+            restart_laptop()
+    except AttributeError:
+        pass
+
+# Listener for keyboard events
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
