19a3de4aa1d6f546c0875eae3b22f231d7b7e6c7
siddhpatel299 <siddhpatel2992001@gmail.com>
2024-08-15 19:18:13 -0700

    server cliend with thread

--- Client.py
+++ Client.py
@@ -1,49 +1,66 @@
+# import socket
+# import MouseControl , KeyStrokes , Restart
+
+
+# def start_client():
+#     # Create a socket object
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+#     # Define server IP address and port
+#     host = '172.20.10.7'
+#     port = 12310
+    
+#     # Connect to the server
+#     client_socket.connect((host, port))
+    
+#     while True:
+        
+#         # Send a message to the server
+#         client_message = input("Client: ")
+#         client_socket.send(client_message.encode('utf-8'))
+        
+#         if client_message.lower() == 'exit':
+#             print("Closing connection")
+#             break
+        
+#         # Receive response from the server
+#         server_message = client_socket.recv(1024).decode('utf-8')
+#         if server_message.lower() == 'exit':
+#             print("Server closed the connection")
+#             break
+        
+#         if server_message == '1':
+#             MouseControl.mouse_attack()
+        
+#         if server_message == '2':
+#             Restart.restart()
+        
+#         if server_message == '3':
+#             KeyStrokes.KeyStrokes()
+        
+#         else:
+#             pass
+#         print(f"Server: {server_message}")
+
+#     # Close the client socket
+#     client_socket.close()
+
+# if __name__ == "__main__":
+#     start_client()
+
 import socket
-import MouseControl , KeyStrokes , Restart
-
-
-def start_client():
-    # Create a socket object
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Define server IP address and port
-    host = 'localhost'
-    port = 12345
-    
-    # Connect to the server
-    client_socket.connect((host, port))
-    
-    while True:
-        
-        # Send a message to the server
-        client_message = input("Client: ")
-        client_socket.send(client_message.encode('utf-8'))
-        
-        if client_message.lower() == 'exit':
-            print("Closing connection")
-            break
-        
-        # Receive response from the server
-        server_message = client_socket.recv(1024).decode('utf-8')
-        if server_message.lower() == 'exit':
-            print("Server closed the connection")
-            break
-        
-        if server_message == '1':
-            MouseControl.mouse_attack()
-        
-        if server_message == '2':
-            Restart.restart()
-        
-        if server_message == '3':
-            KeyStrokes.KeyStrokes()
-        
-        else:
-            pass
-        print(f"Server: {server_message}")
-
-    # Close the client socket
-    client_socket.close()
-
-if __name__ == "__main__":
-    start_client()
+ClientMultiSocket = socket.socket()
+host = '172.20.10.15'
+port = 6001
+print('Waiting for connection response')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+res = ClientMultiSocket.recv(1024)
+while True:
+    Input = input('Hey there: ')
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
--- Server.py
+++ Server.py
@@ -1,45 +1,77 @@
+# import socket
+
+# def start_server():
+#     # Create a socket object
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+#     # Define server host and port
+#     host = 'localhost'
+#     port = 12345
+    
+#     # Bind the socket to the host and port
+#     server_socket.bind((host, port))
+    
+#     # Server listens for connections (max 5 connections in the queue)
+#     server_socket.listen(5)
+#     print("Choose an option for attack: 1- Mouse Attack\n, 2- Restart\n, 3- keyboard monitering\n")
+    
+#     # Accept the client connection
+#     client_socket, addr = server_socket.accept()
+#     print(f"Connected to {addr}")
+    
+#     while True:
+#         # Receive message from the client
+#         client_message = client_socket.recv(1024).decode('utf-8')
+#         if client_message.lower() == 'exit':
+#             print("Client disconnected")
+#             break
+        
+#         print(f"Client: {client_message}")
+        
+#         # Send a response back to the client
+#         server_message = input("Server: ")
+#         client_socket.send(server_message.encode('utf-8'))
+        
+#         if server_message.lower() == 'exit':
+#             print("Closing connection")
+#             break
+
+#     # Close the client socket
+#     client_socket.close()
+#     # Close the server socket
+#     server_socket.close()
+
+# if __name__ == "__main__":
+#     start_server()
+
 import socket
-
-def start_server():
-    # Create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import os
+from _thread import * 
+ServerSideSocket = socket.socket() 
+host = '172.20.10.15'
+port = 6000
+ThreadCount = 0
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e: 
+    print(str(e))
     
-    # Define server host and port
-    host = 'localhost'
-    port = 12345
-    
-    # Bind the socket to the host and port
-    server_socket.bind((host, port))
-    
-    # Server listens for connections (max 5 connections in the queue)
-    server_socket.listen(5)
-    print("Choose an option for attack: 1- Mouse Attack\n, 2- Restart\n, 3- keyboard monitering\n")
-    
-    # Accept the client connection
-    client_socket, addr = server_socket.accept()
-    print(f"Connected to {addr}")
-    
+print('Socket is listening..') 
+ServerSideSocket.listen(5)
+def multi_threaded_client(connection): 
+    connection.send(str.encode('Server is working:')) 
     while True:
-        # Receive message from the client
-        client_message = client_socket.recv(1024).decode('utf-8')
-        if client_message.lower() == 'exit':
-            print("Client disconnected")
+        data = connection.recv(2048).decode('utf-8') 
+        print(data)
+        response = input("Server :")
+        if not data:
             break
-        
-        print(f"Client: {client_message}")
-        
-        # Send a response back to the client
-        server_message = input("Server: ")
-        client_socket.send(server_message.encode('utf-8'))
-        
-        if server_message.lower() == 'exit':
-            print("Closing connection")
-            break
-
-    # Close the client socket
-    client_socket.close()
-    # Close the server socket
-    server_socket.close()
-
-if __name__ == "__main__":
-    start_server()
+    connection.sendall(str.encode(response)) 
+    connection.close()
+while True:
+    Client, address = ServerSideSocket.accept() 
+    print('Connected to: ' + address[0] + ':' + str(address[1])) 
+    start_new_thread(multi_threaded_client, (Client, )) 
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
